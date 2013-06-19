16b63bbedd5f73202fc1621f573b65fdd8ffab61
Simon Ng <simonn@gmail.com>
2013-06-19 10:32:20 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "RoR Sample App001" }
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,8 +11,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "RoR Sample App001 | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
 	end					
   end
   
@@ -21,22 +22,30 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "RoR Sample App001 | Help")
-	end					
-end
+      page.should have_selector('title', :text => "#{base_title} | Help")
+	 end					
+  end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
     end
-    it "should have the title'About Us'" do
+    it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "RoR Sample App001 | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
 	end					
   end
 
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+	end					
+  end
 
 end #End of describ "StaticPages"
